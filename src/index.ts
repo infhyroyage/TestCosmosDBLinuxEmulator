ed3fe1a17035dc7b243992a34fdb315bc1039647
KIDO Takeru <keidy2463@gmail.com>
2022-08-07 01:48:25 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { CosmosClient } from "@azure/cosmos";
+import { Item } from "../types/Item";
 
 const URI = "https://localhost:8081";
 const PRIMARY_KEY =
@@ -6,7 +7,23 @@ const PRIMARY_KEY =
 
 const main = async () => {
   const cosmosClient = new CosmosClient({ endpoint: URI, key: PRIMARY_KEY });
-  await cosmosClient.databases.create({ id: "TestDatabase" });
+
+  const { database } = await cosmosClient.databases.createIfNotExists({
+    id: "TestDatabase",
+  });
+
+  const { container } = await database.containers.createIfNotExists({
+    id: "TestContainer",
+  });
+
+  const newItem: Item = {
+    id: "1",
+    num: 1,
+    name: "one",
+  };
+  const res = await container.items.upsert<Item>(newItem);
+  console.log(res.statusCode);
+  console.log(res.substatus);
 };
 
 main().catch((e) => {
---
+++ types/Item.ts
@@ -0,0 +1,5 @@
+export type Item = {
+  id: string;
+  num: number;
+  name: string;
+};
